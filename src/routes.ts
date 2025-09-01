5c66e5678b66b45d0f02aca91f1a734effedae04
AngeloModanez <angelomodanez15@gmail.com>
2025-09-01 18:22:23 -0300

    :wrench: chore: Better routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,8 @@
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
 export type RootStackParamList = {
   HelloWorld: undefined;
   Login: undefined;
 };
+
+export type NavProps = NativeStackNavigationProp<RootStackParamList>;
