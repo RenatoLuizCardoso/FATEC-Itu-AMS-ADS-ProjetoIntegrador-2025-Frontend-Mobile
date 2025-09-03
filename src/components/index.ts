c72aaaee3c42b8edd7e84998da045a203bc6e95f
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2025-09-03 18:01:43 -0300

    :sparkles: feat: Component counter (#12)

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export * from './bottombar';
 export * from './button';
 export * from './card';
 export * from './container';
+export * from './counter';
 export * from './icon';
 export * from './input';
 export * from './modal';
