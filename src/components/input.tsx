13f91e786abdb9f904edbc32e1c32c777e18f163
AngeloModanez <angelomodanez15@gmail.com>
2025-09-05 09:45:12 -0300

    :lipstick: feat: Addd BackgroundColor white to component input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -13,7 +13,7 @@ export function Input({
       {...props}
       mode={mode ? mode : 'outlined'}
       outlineStyle={[{ borderRadius: 12 }, outlineStyle]}
-      style={[{ width: '80%' }, style]}
+      style={[{ width: '80%', backgroundColor: 'white' }, style]}
       activeOutlineColor={activeOutlineColor ? activeOutlineColor : '#6B031D'}
       outlineColor={outlineColor ? outlineColor : '#676767'}
     />
